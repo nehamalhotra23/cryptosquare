65d6a83abd1f4c4e4e07fad02be0578914120459
nikhil batta and Neha Malhotra <nikhil@>
2019-08-22 16:06:37 -0700

    adding cryptosquare

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
+$(".formOne").submit(function(event){
+  event.preventDefault();
+var userInput = $("#field").val();
+var sliceUp = userInput.slice("");
+var trimmer = sliceUp.replace(/\s/g,"");
+var sentence = []
+var square = Math.sqrt(trimmer.length)
+console.log(square);
+var roundSquare = Math.round(square);
+console.log(roundSquare);
 
+for(i=0;i<trimmer.length;i+=roundSquare){
+  sentence.push(i);
+  $(".formOne").append("<li>" + sentence + "</li>");
 
+ }
+ if(sentence.includes(12) ) {
+   console.log('working');
+ }
 
-
-})
-var arr = []
-var userInput = arr.push("This is a test sentence")
-function columnMaker(){
-  $("#tutorial").
-}
-console.log(arr)
+});
+});
